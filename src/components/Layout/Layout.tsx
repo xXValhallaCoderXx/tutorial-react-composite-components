88ed6529eefb155f9c8d6f53c9a6644516b7dc33
xXValhallaCoderXx <renate.gouveia@gmail.com>
2023-05-08 04:13:58 +0800

    Clean up

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Typography, Grid, Button, ButtonProps } from "@mui/material"
+import { Box, Card, Typography, Grid, Button, ButtonProps, AppBarProps } from "@mui/material"
 import AppBar from "../AppBar/AppBar"
 import React from "react";
 
@@ -15,6 +15,7 @@ interface ListViewWithChildren extends React.FC<ChildProps> {
     Header: HeaderWithChildrenProp
     Content: any;
     Button: React.FC<ButtonProps>
+    AppBar: React.FC<AppBarProps>
 }
 
 interface HeaderWithChildrenProp extends React.FC<ChildProps> {
@@ -35,7 +36,7 @@ interface HeaderLeftProps extends React.FC<ChildProps> {
 }
 
 
-const ListView: ListViewWithChildren = ({ children }) => <div className="flex flex-col w-[100%] rounded">  <AppBar /> <Box p={2}>
+const ListView: ListViewWithChildren = ({ children }) => <div className="flex flex-col w-[100%] rounded"><Box p={2}>
     <Card>{children}</Card></Box></div>
 
 const Header: HeaderWithChildrenProp = ({ children }) => <Grid container justifyContent="space-between" sx={{ padding: 4, borderBottom: "1px solid black" }}>{children}</Grid>
@@ -51,12 +52,12 @@ const Title: React.FC<TitleProps> = ({ children }) => <Typography variant="h3" >
 const PrimaryCTA: React.FC<ButtonProps> = ({ children }) => <Button sx={{ height: 40 }} variant="contained" size="small" >{children}</Button>
 const SecondaryCTA: React.FC<ButtonProps> = ({ children }) => <Button sx={{ height: 40 }} variant="outlined" >{children}</Button>
 
-const RandomButton: React.FC<ButtonProps> = ({ children, ...rest }) => <Button sx={{ height: 40 }} {...rest} >{children}</Button>
+// const RandomButton: React.FC<ButtonProps> = ({ children, ...rest }) => <Button sx={{ height: 40 }} {...rest} >{children}</Button>
 
 ListView.Header = Header
 ListView.Content = ListViewContent
-
-ListView.Button = RandomButton
+ListView.AppBar = AppBar
+ListView.Button = Button
 Header.Left = HeaderLeft
 Header.Right = HeaderRight
 HeaderLeft.Title = Title
